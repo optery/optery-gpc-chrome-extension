258adeaa3985f799b7e82febdefc12e98b9b5855
detail.wes <details.wes@gmail.com>
2024-05-28 17:30:47 +0530

    feat: Add functionality for GPC extension

---
+++ public/background.js
@@ -0,0 +1,34 @@
+// Listen for message from content script
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "initialGPCValue") {
+    chrome.storage.sync.get("gpcValue", (result) => {
+      if (result.gpcValue === undefined) {
+        chrome.storage.sync.set({ gpcValue: message.value });
+      }
+    });
+  } else if (message.action === "updateRulesetState") {
+    const { enable } = message;
+    chrome.declarativeNetRequest.updateEnabledRulesets(
+      {
+        enableRulesetIds: enable ? ["ruleset_1"] : [],
+        disableRulesetIds: enable ? [] : ["ruleset_1"],
+      },
+      () => {
+        if (chrome.runtime.lastError) {
+          console.error(
+            "Failed to update ruleset state:",
+            chrome.runtime.lastError
+          );
+        }
+        sendResponse({ success: !chrome.runtime.lastError });
+      }
+    );
+    return true;
+  } else if (message.action === "getGPCValue") {
+    chrome.storage.sync.get("gpcValue", (result) => {
+      const gpcValue = result.gpcValue;
+      sendResponse({ gpcValue });
+    });
+  }
+  return true;
+});
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /* global chrome */
 import { useEffect, useState } from "react";
+import Popup from "./components/Popup";
 
 const App = () => {
   const [gpcEnabled, setGpcEnabled] = useState(false);
@@ -22,6 +23,7 @@ const App = () => {
     };
   }, []);
 
+  // Function to handle the change of GPC status
   const handleGpcChange = () => {
     const isChecked = !gpcEnabled;
     setGpcEnabled(isChecked);
@@ -40,82 +42,9 @@ const App = () => {
   };
 
   return (
-      <div className={"flex justify-center items-center bg-[#FFFFFF] h-full"}>
-        <div className={`p-5 bg-[#011E3B] w-[457px] h-[657px] flex flex-col`}>
-          <div className={"flex flex-col gap-8"}>
-            <div className={"flex gap-2 justify-center items-center"}>
-              <img src={"/icons/optery.svg"} alt="Optery" />
-              <div
-                  className={
-                    "bg-[#B5F8F1] text-[#011E3B] text-center text-xs not-italic font-semibold uppercase rounded-[19.567px] py-2.5 px-3.5"
-                  }
-              >
-                extension
-              </div>
-            </div>
-            <div>
-              <div className={"flex flex-col justify-center items-center gap-4"}>
-              <span className={"font-semibold text-xl text-[#B5F8F1]"}>
-                Global Privacy Control
-              </span>
-                <div
-                    className={"flex flex-col justify-center items-center gap-2"}
-                >
-                  <p
-                      className={
-                        "not-italic font-normal text-sm text-[#F8FAFB] text-center"
-                      }
-                  >
-                    Optery for Open Privacy Extension emits Global Privacy Control
-                    signals to websites restricting the storage of your personal
-                    information.
-                  </p>
-                  <button
-                      className={
-                        "flex items-center text-sm not-italic font-semibold underline decoration-1 text-[#F8FAFB]"
-                      }
-                      onClick={() =>
-                          window.open("https://globalprivacycontrol.org/", "_blank")
-                      }
-                  >
-                    Learn more about GPC
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={`flex justify-center items-center`}>
-            <button onClick={handleGpcChange}>
-              {gpcEnabled ? (
-                  <img
-                      src={"/icons/shieldon.svg"}
-                      alt="Shield On"
-                      className={"h-[333px] w-[278px]"}
-                  />
-              ) : (
-                  <img
-                      src={"/icons/shieldoff.svg"}
-                      alt="Shield Off"
-                      className={"h-[333px] w-[194px]"}
-                  />
-              )}
-            </button>
-          </div>
-          <div className={"flex justify-center items-center flex-col gap-4"}>
-            <button
-                className={
-                  "text-xs font-semibold not-italic bg-[#4D81F1] text-[#FFFFFF] rounded py-2.5 px-9"
-                }
-                onClick={() => window.open("https://www.optery.com/", "_blank")}
-            >
-              Optery Dashboard
-            </button>
-            <button className={"text-xs font-semibold not-italic text-[#FFFFFF]"}>
-              Learn more about Optery
-            </button>
-          </div>
-        </div>
-      </div>
+    <>
+      <Popup handleGpcChange={handleGpcChange} gpcEnabled={gpcEnabled} />
+    </>
   );
 };
 
